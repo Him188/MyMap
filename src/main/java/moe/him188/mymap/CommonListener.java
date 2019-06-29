3278f9ea569039c050260bb3c73ceecc1b63fd7c
Him188moe <Him188@mamoe.net>
2019-06-30 00:42:41 +0800

    Updated! Fixed bugs and now it runs on newest NukkitX

--- src/main/java/moe/him188/mymap/CommonListener.java
+++ src/main/java/moe/him188/mymap/CommonListener.java
@@ -7,9 +7,13 @@ import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.entity.EntityLevelChangeEvent;
 import cn.nukkit.event.player.PlayerJoinEvent;
+import cn.nukkit.event.player.PlayerMoveEvent;
 import cn.nukkit.event.server.DataPacketReceiveEvent;
 import cn.nukkit.network.protocol.MapInfoRequestPacket;
 import cn.nukkit.scheduler.AsyncTask;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * 用于画框更新等的普通事件监听器
@@ -38,20 +42,29 @@ public final class CommonListener implements Listener {
         }
     }
 
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-    public void onJoin(PlayerJoinEvent event) {
-        Server.getInstance().getScheduler().scheduleAsyncTask(this.plugin, new AsyncTask() {
-            @Override
-            public void onRun() {
-                for (MyMapFrame frame : MyMap.getInstance().getList()) {
-                    frame.getImageUpdater().requestBlockUpdate(event.getPlayer(), event.getPlayer().getLevel());
-                    frame.getImageUpdater().requestMapUpdate(event.getPlayer(), true);
+    private List<Long> playersNeedUpdate = new LinkedList<>();
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onMove(PlayerMoveEvent event) {
+        if (playersNeedUpdate.remove(event.getPlayer().getId())) {
+            Server.getInstance().getScheduler().scheduleAsyncTask(this.plugin, new AsyncTask() {
+                @Override
+                public void onRun() {
+                    for (MyMapFrame frame : MyMap.getInstance().getList()) {
+                        frame.getImageUpdater().requestBlockUpdate(event.getPlayer(), event.getPlayer().getLevel());
+                        frame.getImageUpdater().requestMapUpdate(event.getPlayer(), true);
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 
-    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onJoin(PlayerJoinEvent event) {
+        playersNeedUpdate.add(event.getPlayer().getId());
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     public void onPacketReceive(DataPacketReceiveEvent event) {
         if (event.getPacket() instanceof MapInfoRequestPacket) {
             for (MyMapFrame frame : MyMap.getInstance().getList()) {
